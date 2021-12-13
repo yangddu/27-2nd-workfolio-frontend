490673453d2ccacbba318568121e5d3a65e27cb8
yuryang Kim <flowratekim@gmail.com>
2021-12-13 23:22:52 +0900

    Add : Footer Layout completed

---
+++ src/components/Footer/FooterData.js
@@ -0,0 +1,41 @@
+export const FOOTER_DATA_1 = [
+  {
+    id: 1,
+    value: '상호: (주)오피스 폴리오',
+  },
+  {
+    id: 2,
+    value: '서울특별시 강남구 테헤란로',
+  },
+  {
+    id: 3,
+    value: 'help@officfolio',
+  },
+  {
+    id: 4,
+    value: 'ⓒ >OFFICEFOLIO',
+  },
+];
+
+export const FOOTER_DATA_2 = [
+  {
+    id: 1,
+    value: 'ABOUT',
+  },
+  {
+    id: 2,
+    value: '4POINT OF VIEW',
+  },
+  {
+    id: 3,
+    value: 'CONTACT US',
+  },
+  {
+    id: 4,
+    value: '이용약관',
+  },
+  {
+    id: 5,
+    value: '개인정보 처리방침',
+  },
+];
