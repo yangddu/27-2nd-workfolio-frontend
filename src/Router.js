c379a27a4ae3d6033f6abd2383e9f7e608b24e73
yuryang Kim <flowratekim@gmail.com>
2021-12-20 13:49:45 +0900

    "Modify : conflict solved,  Nav Layout "

--- src/Router.js
+++ src/Router.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login/Login';
 import Footer from './components/Footer/Footer';
+import Nav from './components/Nav/Nav';
 
 function Router() {
   return (
     <BrowserRouter>
+      <Nav />
       <Routes>
         <Route path="/login" element={<Login />} />
       </Routes>
